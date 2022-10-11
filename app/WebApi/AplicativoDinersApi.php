1483afc3826c71c3437581330268c9244eb6eea6
Andres Garcia <andresg@apiconsultoria.com>
2022-10-11 16:17:41 -0500

    api

--- app/WebApi/AplicativoDinersApi.php
+++ app/WebApi/AplicativoDinersApi.php
@@ -903,4 +903,404 @@ class AplicativoDinersApi extends BaseController {
 		return $this->json($res->conDatos($retorno));
 	}
 
+	/**
+	 * campos_tarjeta_discover
+	 * @param $producto_id
+	 * @param $session
+	 */
+	function campos_tarjeta_discover() {
+		if(!$this->isPost()) return "campos_tarjeta_discover";
+		$res = new RespuestaConsulta();
+		$producto_id = $this->request->getParam('producto_id');
+		$session = $this->request->getParam('session');
+		$user = UsuarioLogin::getUserBySession($session);
+
+		$tarjeta_discover = AplicativoDiners::getAplicativoDinersDetalle('DISCOVER',$producto_id);
+
+		$seccion1['nombre'] = 'DISCOVER';
+		$seccion1['colorFondo'] = '#E66929';
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'CICLO',
+			'valor' => $tarjeta_discover['ciclo'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'EDAD DE CARTERA',
+			'valor' => $tarjeta_discover['edad_cartera'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'CODIGO DE CANCELACION',
+			'valor' => $tarjeta_discover['codigo_cancelacion'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'CODIGO DE BOLETIN',
+			'valor' => $tarjeta_discover['codigo_boletin'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'DÉBITO AUTOMÁTICO',
+			'valor' => $tarjeta_discover['debito_automatico'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'PROMEDIO DE PAGO',
+			'valor' => $tarjeta_discover['promedio_pago'],
+			'tipo' => 'label',
+			'colorFondo' => '#c3c3c3'
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'SCORE',
+			'valor' => $tarjeta_discover['score'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'CAMPAÑA',
+			'valor' => $tarjeta_discover['campana'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'EJECUTIVO ACTUAL A CARGO DE CUENTA',
+			'valor' => $tarjeta_discover['ejecutivo_actual_cuenta'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'LUGAR DE TRABAJO',
+			'valor' => $tarjeta_discover['lugar_trabajo'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'FECHA ÚLTIMA GESTIÓN',
+			'valor' => $tarjeta_discover['fecha_ultima_gestion'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'MOTIVO DE GESTIÓN',
+			'valor' => $tarjeta_discover['motivo_gestion'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'DESCRIPCIÓN DE GESTIÓN',
+			'valor' => $tarjeta_discover['descripcion_gestion'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'OBSERVACIONES DE GESTIÓN',
+			'valor' => $tarjeta_discover['observacion_gestion'],
+			'tipo' => 'text',
+			'name' => 'data[observacion_gestion]'
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'FECHA DE COMPROMISO',
+			'valor' => $tarjeta_discover['fecha_compromiso'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'TT/EXIG/PARCIAL',
+			'valor' => $tarjeta_discover['tt_exig_parcial'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'MOTIVO DE NO PAGO ANTERIOR',
+			'valor' => $tarjeta_discover['motivo_no_pago_anterior'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'FINANCIAMIENTO VIGENTE',
+			'valor' => $tarjeta_discover['financiamiento_vigente'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'N° CUOTAS PENDIENTES',
+			'valor' => $tarjeta_discover['numero_cuotas_pendientes'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'TT CUOTAS FACT',
+			'valor' => $tarjeta_discover['tt_cuotas_fact'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'VALOR CUOTAS PENDIENTES',
+			'valor' => $tarjeta_discover['valor_cuotas_pendientes'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'VALOR DE CUOTA',
+			'valor' => $tarjeta_discover['valor_cuota'],
+			'tipo' => 'label',
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'SEGUNDA REESTRUCTURACIÓN',
+			'valor' => $tarjeta_discover['segunda_restructuracion'],
+			'tipo' => 'label',
+			'colorFondo' => '#c3c3c3'
+		];
+		$seccion1['contenido'][] = [
+			'etiqueta' => 'TOTAL RIESGO',
+			'valor' => $tarjeta_discover['total_riesgo'],
+			'tipo' => 'label',
+			'colorFondo' => '#c3c3c3'
+		];
+
+		$seccion2['nombre'] = 'SALDOS FACTURADOS';
+		$seccion2['colorFondo'] = '#E66929';
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'SALDO A 90 Y MAS 90 DÍAS',
+			'valor' => $tarjeta_discover['saldo_90_facturado'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_90_facturado]',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'SALDO A 60 DIAS',
+			'valor' => $tarjeta_discover['saldo_60_facturado'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_60_facturado]',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'SALDO A 30 DIAS',
+			'valor' => $tarjeta_discover['saldo_30_facturado'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_30_facturado]',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'SALDO ACTUALES',
+			'valor' => $tarjeta_discover['saldo_actual_facturado'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_actual_facturado]',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'DEUDA ACTUAL',
+			'valor' => $tarjeta_discover['deuda_actual'],
+			'tipo' => 'label',
+			'name' => 'data[deuda_actual]',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'INTERESES FACTURADOS',
+			'valor' => $tarjeta_discover['interes_facturado'],
+			'tipo' => 'label',
+			'name' => 'data[interes_facturado]',
+			'colorFondo' => '#c3c3c3',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'NUMERO DE DIFERIDOS FACTURADOS',
+			'valor' => $tarjeta_discover['numero_diferidos_facturados'],
+			'tipo' => 'label',
+			'name' => 'data[numero_diferidos_facturados]',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'TOTAL VALOR PRE CANCELACION DIFERIDOS',
+			'valor' => $tarjeta_discover['total_precancelacion_diferidos'],
+			'tipo' => 'label',
+			'name' => 'data[total_precancelacion_diferidos]',
+		];
+		$seccion2['contenido'][] = [
+			'etiqueta' => 'ESPECIALIDAD VENTA VEHICULOS',
+			'valor' => $tarjeta_discover['especialidad_venta_vehiculos'],
+			'tipo' => 'label',
+			'name' => 'data[especialidad_venta_vehiculos]',
+		];
+
+		$seccion3['nombre'] = 'PAGOS';
+		$seccion3['colorFondo'] = '#E66929';
+		$seccion3['contenido'][] = [
+			'etiqueta' => 'ABONO EFECTIVO DEL SISTEMA',
+			'valor' => $tarjeta_discover['abono_efectivo_sistema'],
+			'tipo' => 'label',
+			'name' => 'data[abono_efectivo_sistema]',
+			'colorFondo' => '#c3c3c3',
+		];
+		$seccion3['contenido'][] = [
+			'etiqueta' => 'ABONO NEGOCIADOR',
+			'valor' => $tarjeta_discover['abono_negociador'],
+			'tipo' => 'number',
+			'name' => 'data[abono_negociador]',
+			'colorFondo' => '#c3c3c3',
+		];
+		$seccion3['contenido'][] = [
+			'etiqueta' => 'ABONO TOTAL',
+			'valor' => $tarjeta_discover['abono_total'],
+			'tipo' => 'label',
+			'name' => 'data[abono_total]',
+			'colorFondo' => '#c3c3c3',
+		];
+
+		$seccion4['nombre'] = 'SALDOS FACTURADOS DESPUÉS DE ABONO';
+		$seccion4['colorFondo'] = '#E66929';
+		$seccion4['contenido'][] = [
+			'etiqueta' => 'SALDO A 90 Y MAS 90 DIAS',
+			'valor' => $tarjeta_discover['saldo_90_facturado_despues_abono'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_90_facturado_despues_abono]',
+		];
+		$seccion4['contenido'][] = [
+			'etiqueta' => 'SALDO A 60 DIAS',
+			'valor' => $tarjeta_discover['saldo_60_facturado_despues_abono'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_60_facturado_despues_abono]',
+		];
+		$seccion4['contenido'][] = [
+			'etiqueta' => 'SALDO A 30 DIAS',
+			'valor' => $tarjeta_discover['saldo_30_facturado_despues_abono'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_30_facturado_despues_abono]',
+		];
+		$seccion4['contenido'][] = [
+			'etiqueta' => 'SALDO ACTUALES',
+			'valor' => $tarjeta_discover['saldo_actual_facturado_despues_abono'],
+			'tipo' => 'label',
+			'name' => 'data[saldo_actual_facturado_despues_abono]',
+		];
+
+		$seccion5['nombre'] = 'VALORES POR FACTURAR';
+		$seccion5['colorFondo'] = '#E66929';
+		$seccion5['contenido'][] = [
+			'etiqueta' => 'INTERESES POR FACTURAR',
+			'valor' => $tarjeta_discover['interes_facturar'],
+			'tipo' => 'label',
+			'name' => 'data[interes_facturar]',
+		];
+		$seccion5['contenido'][] = [
+			'etiqueta' => 'CORRIENTES POR FACTURAR',
+			'valor' => $tarjeta_discover['corrientes_facturar'],
+			'tipo' => 'label',
+			'name' => 'data[corrientes_facturar]',
+		];
+		$seccion5['contenido'][] = [
+			'etiqueta' => 'ND POR FACTURAR',
+			'valor' => $tarjeta_discover['nd_facturar'],
+			'tipo' => 'label',
+			'name' => 'data[nd_facturar]',
+		];
+		$seccion5['contenido'][] = [
+			'etiqueta' => 'NC POR FACTURAR',
+			'valor' => $tarjeta_discover['nc_facturar'],
+			'tipo' => 'label',
+			'name' => 'data[nc_facturar]',
+		];
+		$seccion5['contenido'][] = [
+			'etiqueta' => 'GASTOS DE COBRANZA / OTROS',
+			'valor' => $tarjeta_discover['gastos_cobranza'],
+			'tipo' => 'number',
+			'name' => 'data[gastos_cobranza]',
+		];
+		$seccion5['contenido'][] = [
+			'etiqueta' => 'VALOR OTRAS TARJETAS',
+			'valor' => $tarjeta_discover['valor_otras_tarjetas'],
+			'tipo' => 'number',
+			'name' => 'data[valor_otras_tarjetas]',
+		];
+
+		$seccion6['nombre'] = 'FINANCIAMIENTO';
+		$seccion6['colorFondo'] = '#E66929';
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'TIPO DE FINANCIAMIENTO',
+			'valor' => $tarjeta_discover['tipo_financiamiento'],
+			'tipo' => 'label',
+			'name' => 'data[tipo_financiamiento]',
+			'colorFondo' => '#E66929',
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'TOTAL',
+			'valor' => $tarjeta_discover['total_financiamiento'],
+			'tipo' => 'label',
+			'name' => 'data[total_financiamiento]',
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'EXIGIBLE',
+			'valor' => $tarjeta_discover['exigible_financiamiento'],
+			'tipo' => 'choice',
+			'name' => 'data[exigible_financiamiento]',
+			'choices' => [['id' => 'SI','label' => 'SI'],['id' => 'NO','label' => 'NO']],
+		];
+		$plazo_financiamiento = [
+			['id' => '','label' => ''],
+			['id' => '2','label' => '2'],
+			['id' => '3','label' => '3'],
+			['id' => '4','label' => '4'],
+			['id' => '5','label' => '5'],
+			['id' => '6','label' => '6'],
+			['id' => '7','label' => '7'],
+			['id' => '8','label' => '8'],
+			['id' => '9','label' => '9'],
+			['id' => '12','label' => '12'],
+			['id' => '13','label' => '13'],
+			['id' => '14','label' => '14'],
+			['id' => '15','label' => '15'],
+			['id' => '16','label' => '16'],
+			['id' => '17','label' => '17'],
+			['id' => '18','label' => '18'],
+			['id' => '19','label' => '19'],
+			['id' => '20','label' => '20'],
+			['id' => '21','label' => '21'],
+			['id' => '22','label' => '22'],
+			['id' => '23','label' => '23'],
+			['id' => '24','label' => '24'],
+			['id' => '30','label' => '30'],
+			['id' => '36','label' => '36'],
+			['id' => '48','label' => '48'],
+			['id' => '60','label' => '60'],
+			['id' => '72','label' => '72'],
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'PLAZO DE FINANCIAMIENTO',
+			'valor' => $tarjeta_discover['plazo_financiamiento'],
+			'tipo' => 'choice',
+			'name' => 'data[plazo_financiamiento]',
+			'choices' => $plazo_financiamiento,
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'MOTIVO DE NO PAGO',
+			'valor' => $tarjeta_discover['motivo_no_pago'],
+			'tipo' => 'text',
+			'name' => 'data[motivo_no_pago]',
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'NÚMERO DE MESES DE GRACIA',
+			'valor' => $tarjeta_discover['numero_meses_gracia'],
+			'tipo' => 'number',
+			'name' => 'data[numero_meses_gracia]',
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'VALOR A FINANCIAR',
+			'valor' => $tarjeta_discover['valor_financiar'],
+			'tipo' => 'label',
+			'name' => 'data[valor_financiar]',
+			'colorFondo' => '#E66929',
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'TOTAL INTERESES',
+			'valor' => $tarjeta_discover['total_intereses'],
+			'tipo' => 'label',
+			'name' => 'data[total_intereses]',
+			'colorFondo' => '#E66929',
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'TOTAL FINANCIAMIENTO',
+			'valor' => $tarjeta_discover['total_financiamiento_total'],
+			'tipo' => 'label',
+			'name' => 'data[total_financiamiento_total]',
+			'colorFondo' => '#E66929',
+		];
+		$seccion6['contenido'][] = [
+			'etiqueta' => 'VALOR CUOTA MENSUAL',
+			'valor' => $tarjeta_discover['valor_cuota_mensual'],
+			'tipo' => 'label',
+			'name' => 'data[valor_cuota_mensual]',
+			'colorFondo' => '#E66929',
+		];
+
+		$retorno['secciones'][] = $seccion1;
+		$retorno['secciones'][] = $seccion2;
+		$retorno['secciones'][] = $seccion3;
+		$retorno['secciones'][] = $seccion4;
+		$retorno['secciones'][] = $seccion5;
+		$retorno['secciones'][] = $seccion6;
+
+//		printDie(json_encode($retorno,JSON_PRETTY_PRINT));
+
+		return $this->json($res->conDatos($retorno));
+	}
+
 }
