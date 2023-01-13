4a16d001c385f831a1dcdb6c13752d60e41ab3e9
Andres Garcia <andresg@apiconsultoria.com>
2023-01-13 12:09:47 -0500

    cargar asignaciones

---
+++ app/Models/AplicativoDinersAsignaciones.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @package Models
+ *
+ * @property integer id
+ * @property integer cliente_id
+ * @property string fecha_inicio
+ * @property string fecha_fin
+ * @property integer mes
+ * @property integer anio
+ * @property string campana
+ * @property string marca
+ * @property integer ciclo
+ * @property string nombre_socio
+ * @property string cedula_socio
+ * @property string estado
+ * @property string campos
+ * @property string fecha_ingreso
+ * @property string fecha_modificacion
+ * @property integer usuario_ingreso
+ * @property integer usuario_modificacion
+ * @property boolean eliminado
+ */
+class AplicativoDinersAsignaciones extends Model
+{
+	protected $table = 'aplicativo_diners_asignaciones';
+	const CREATED_AT = 'fecha_ingreso';
+	const UPDATED_AT = 'fecha_modificacion';
+	protected $guarded = [];
+	public $timestamps = false;
+
+	/**
+	 * @param $id
+	 * @param array $relations
+	 * @return mixed|AplicativoDinersSaldos
+	 */
+	static function porId($id, $relations = [])
+	{
+		$q = self::query();
+		if($relations)
+			$q->with($relations);
+		return $q->findOrFail($id);
+	}
+
+	static function eliminar($id)
+	{
+		$q = self::porId($id);
+		$q->eliminado = 1;
+		$q->usuario_modificacion = \WebSecurity::getUserData('id');
+		$q->fecha_modificacion = date("Y-m-d H:i:s");
+		$q->save();
+		return $q;
+	}
+
+	static function getTodos() {
+		$pdo = self::query()->getConnection()->getPdo();
+		$db = new \FluentPDO($pdo);
+
+		$q = $db->from('aplicativo_diners_asignaciones ads')
+			->innerJoin('cliente cl ON cl.id = ads.cliente_id')
+			->select(null)
+			->select('ads.*, cl.cedula')
+			->where('ads.eliminado',0);
+		$lista = $q->fetchAll();
+		$retorno = [];
+		foreach ($lista as $l){
+			$retorno[] = $l;
+		}
+		return $retorno;
+	}
+
+}
