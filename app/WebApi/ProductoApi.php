89a2a5cbc0ba895e2bbd2f702b42060870844e4d
Andres Garcia <andresg@apiconsultoria.com>
2023-02-28 09:41:10 -0500

    api

--- app/WebApi/ProductoApi.php
+++ app/WebApi/ProductoApi.php
@@ -355,11 +355,11 @@ class ProductoApi extends BaseController
 		$res = new RespuestaConsulta();
 
 		$q = $this->request->getParam('q');
-//		\Auditor::info('buscar_listas_n4 q: '.$q, 'API', $q);
+		\Auditor::info('buscar_listas_n4 q: '.$q, 'API', $q);
 		$page = $this->request->getParam('page');
-//		\Auditor::info('buscar_listas_n4 page: '.$page, 'API', $page);
+		\Auditor::info('buscar_listas_n4 page: '.$page, 'API', $page);
 		$data = $this->request->getParam('data');
-//		\Auditor::info('buscar_listas_n4 data: '.$data, 'API', $data);
+		\Auditor::info('buscar_listas_n4 data: '.$data, 'API', $data);
 		$session = $this->request->getParam('session');
 		$user = UsuarioLogin::getUserBySession($session);
 
