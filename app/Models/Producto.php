a9a78e7b10b0ecbf7157a5e6af05c5ec35fea121
Andres Garcia <andresg@apiconsultoria.com>
2023-06-22 09:44:44 -0500

    test

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -595,6 +595,7 @@ class Producto extends Model
 		if($data['nc_facturar'] > 0) {
 			$nc_facturar = $data['nc_facturar'];
 		}
+		\Auditor::info('valor_financiar1: '.$data['valor_financiar'], 'API', []);
 		if($data['exigible_financiamiento'] == 'SI') {
 			$data['total_financiamiento'] = 'NO';
 			$data['valor_financiar'] = number_format($deuda_actual, 2, '.', '');
@@ -603,6 +604,8 @@ class Producto extends Model
 			$valor_financiar_diners = $deuda_actual + $total_precancelacion_diferidos + $interes_facturar + $corrientes_facturar + $gastos_cobranza + $valor_otras_tarjetas - $abono_total;
 			$data['valor_financiar'] = number_format($valor_financiar_diners, 2, '.', '');
 		}
+
+		\Auditor::info('valor_financiar2: '.$data['valor_financiar'], 'API', []);
 
 		//CALCULO DE GASTOS DE COBRANZA
 		if($total_precancelacion_diferidos > 0) {
