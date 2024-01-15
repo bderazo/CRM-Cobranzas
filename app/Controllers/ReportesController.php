cdc8c56a8a575487c798256d4b333eff89a57ad5
Andres Garcia <andresg@apiconsultoria.com>
2024-01-15 08:48:18 -0500

    test

--- app/Controllers/ReportesController.php
+++ app/Controllers/ReportesController.php
@@ -370,6 +370,228 @@ class ReportesController extends BaseController
         $this->exportSimple($lista, 'BASE GENERAL', 'base_general' . date("Y-m-d H-i-s") . '.xlsx');
     }
 
+    function excelBaseGeneral($jsonExcel)
+    {
+        \WebSecurity::secure('reportes.base_general');
+
+        $jsonExcel = str_replace('canal_usuario[]', 'canal_usuario', $jsonExcel);
+        $jsonExcel = str_replace('campana_ece[]', 'campana_ece', $jsonExcel);
+        $jsonExcel = str_replace('campana_usuario[]', 'campana_usuario', $jsonExcel);
+        $jsonExcel = str_replace('plaza_usuario[]', 'plaza_usuario', $jsonExcel);
+        $jsonExcel = str_replace('ciclo[]', 'ciclo', $jsonExcel);
+        $jsonExcel = str_replace('resultado[]', 'resultado', $jsonExcel);
+        $jsonExcel = str_replace('accion[]', 'accion', $jsonExcel);
+        $jsonExcel = str_replace('descripcion[]', 'descripcion', $jsonExcel);
+        $jsonExcel = str_replace('gestor[]', 'gestor', $jsonExcel);
+        $jdata = json_decode(htmlspecialchars_decode($jsonExcel), true);
+        $filtros = $jdata['filtros'];
+//        printDie($filtros);
+        $rep = new BaseGeneral($this->get('pdo'));
+        $data = $rep->exportar($filtros);
+
+
+
+//        $data = json_decode($json, true);
+        $lista = [];
+        foreach ($data['data'] as $d) {
+            $aux['NOMBRE SOCIO'] = [
+                'valor' => $d['nombres'],
+                'formato' => 'text'
+            ];
+            $aux['CEDULA'] = [
+                'valor' => $d['cedula'],
+                'formato' => 'text'
+            ];
+            $aux['TELEFONO ULTIMO CONTACTO'] = [
+                'valor' => $d['telefono_contacto'],
+                'formato' => 'text'
+            ];
+            $aux['MARCA'] = [
+                'valor' => $d['tarjeta'],
+                'formato' => 'text'
+            ];
+            $aux['CÓDIGO DE OPERACIÓN'] = [
+                'valor' => $d['codigo_operacion'],
+                'formato' => 'text'
+            ];
+            $aux['ZONA'] = [
+                'valor' => $d['zona'],
+                'formato' => 'text'
+            ];
+            $aux['CIUDAD'] = [
+                'valor' => $d['ciudad'],
+                'formato' => 'text'
+            ];
+
+
+            $aux['TIPO DE CAMPAÑA'] = [
+                'valor' => $d['tipo_campana'],
+                'formato' => 'text'
+            ];
+            $aux['EJECUTIVO'] = [
+                'valor' => $d['ejecutivo'],
+                'formato' => 'text'
+            ];
+            $aux['CICLO'] = [
+                'valor' => $d['ciclo'],
+                'formato' => 'number'
+            ];
+            $aux['EDAD REAL'] = [
+                'valor' => $d['edad'],
+                'formato' => 'number'
+            ];
+            $aux['EDAD FACTURADA'] = [
+                'valor' => $d['edad_asignacion'],
+                'formato' => 'number'
+            ];
+            $aux['TOTAL ASIGNADO'] = [
+                'valor' => $d['total_asignado'],
+                'formato' => 'number'
+            ];
+            $aux['SALDO TOTAL DEUDA'] = [
+                'valor' => $d['saldo_total_deuda'],
+                'formato' => 'number'
+            ];
+            $aux['RIESGO TOTAL'] = [
+                'valor' => $d['riesgo_total'],
+                'formato' => 'number'
+            ];
+            $aux['INTERESES TOTAL'] = [
+                'valor' => $d['interes_total'],
+                'formato' => 'number'
+            ];
+            $aux['RECUPERADO'] = [
+                'valor' => $d['recuperado'],
+                'formato' => 'number'
+            ];
+            $aux['PAGO MINIMO'] = [
+                'valor' => $d['pago_minimo'],
+                'formato' => 'number'
+            ];
+            $aux['FECHA MAXIMA PAGO'] = [
+                'valor' => $d['fecha_maxima_pago'],
+                'formato' => 'text'
+            ];
+            $aux['NUMERO DIFERIDOS'] = [
+                'valor' => $d['numero_diferidos'],
+                'formato' => 'number'
+            ];
+            $aux['NUMERO DE REFINANCIACIONES HISTORICA'] = [
+                'valor' => $d['numero_refinanciaciones_historica'],
+                'formato' => 'number'
+            ];
+            $aux['PLAZO DE FINANCIAMIENTO ACTUAL'] = [
+                'valor' => $d['plazo_financiamiento_actual'],
+                'formato' => 'number'
+            ];
+            $aux['MOTIVO CIERRE'] = [
+                'valor' => $d['motivo_cierre'],
+                'formato' => 'text'
+            ];
+            $aux['OFERTA VALOR'] = [
+                'valor' => $d['oferta_valor'],
+                'formato' => 'text'
+            ];
+            $aux['PENDIENTE ACTUALES'] = [
+                'valor' => $d['pendiente_actuales'],
+                'formato' => 'number'
+            ];
+            $aux['PENDIENTE 30 DIAS'] = [
+                'valor' => $d['pendiente_30'],
+                'formato' => 'number'
+            ];
+            $aux['PENDIENTE 60 DIAS'] = [
+                'valor' => $d['pendiente_60'],
+                'formato' => 'number'
+            ];
+            $aux['PENDIENTE 90 DIAS'] = [
+                'valor' => $d['pendiente_90'],
+                'formato' => 'number'
+            ];
+            $aux['PENDIENTE MAS 90 DIAS'] = [
+                'valor' => $d['pendiente_mas_90'],
+                'formato' => 'number'
+            ];
+            $aux['CRÉDITO INMEDIATO'] = [
+                'valor' => $d['credito_inmediato'],
+                'formato' => 'text'
+            ];
+            $aux['PRODUCTO'] = [
+                'valor' => $d['producto'],
+                'formato' => 'text'
+            ];
+
+
+            $aux['RESULTADO'] = [
+                'valor' => $d['nivel_1_texto'],
+                'formato' => 'text'
+            ];
+            $aux['ACCION'] = [
+                'valor' => $d['nivel_2_texto'],
+                'formato' => 'text'
+            ];
+            $aux['DESCRIPCIÓN'] = [
+                'valor' => $d['nivel_3_texto'],
+                'formato' => 'text'
+            ];
+            $aux['OBSERVACION'] = [
+                'valor' => $d['observaciones'],
+                'formato' => 'text'
+            ];
+            $aux['MOTIVO DE NO PAGO'] = [
+                'valor' => $d['nivel_1_motivo_no_pago_texto'],
+                'formato' => 'text'
+            ];
+            $aux['SUB MOTIVO'] = [
+                'valor' => $d['nivel_2_motivo_no_pago_texto'],
+                'formato' => 'text'
+            ];
+            $aux['FECHA PROMESA DE PAGO'] = [
+                'valor' => $d['fecha_compromiso_pago'],
+                'formato' => 'text'
+            ];
+            $aux['EJECUTIVO'] = [
+                'valor' => $d['gestor'],
+                'formato' => 'text'
+            ];
+            $aux['HORA DE GESTION'] = [
+                'valor' => $d['hora_gestion'],
+                'formato' => 'text'
+            ];
+            $aux['FECHA DE GESTION'] = [
+                'valor' => $d['fecha_gestion'],
+                'formato' => 'text'
+            ];
+            $aux['VALOR COMPROMETIDO'] = [
+                'valor' => $d['valor_comprometido'],
+                'formato' => 'number'
+            ];
+            $aux['GEOREFERENCIA'] = [
+                'valor' => $d['georeferencia'],
+                'formato' => 'text'
+            ];
+            $aux['TIPO NEGOCIACION'] = [
+                'valor' => $d['tipo_negociacion'],
+                'formato' => 'text'
+            ];
+
+            $aux['TIPO GESTIÓN'] = [
+                'valor' => $d['identificador'],
+                'formato' => 'text'
+            ];
+            $aux['ORIGEN GESTIÓN'] = [
+                'valor' => $d['origen'],
+                'formato' => 'text'
+            ];
+            $aux['PESO PALETA'] = [
+                'valor' => $d['peso_paleta'],
+                'formato' => 'text'
+            ];
+            $lista[] = $aux;
+        }
+        $this->exportSimple($lista, 'BASE GENERAL', 'base_general' . date("Y-m-d H-i-s") . '.xlsx');
+    }
+
     //MEJOR Y ULTIMA GESTION
     function mejorUltimaGestion()
     {
